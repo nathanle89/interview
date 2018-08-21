407eb7ff0502c5fd7dd0188a3b3908f394120640
Nguyen Le <nathanle89@gmail.com>
2018-08-20 22:38:16 -0700

    Implement isPalindrome

--- algorithms/strings/palindrome.py
+++ algorithms/strings/palindrome.py
@@ -7,28 +7,19 @@ class Solution(object):
         leading_index = 0
         trailing_index = len(s) - 1
 
-        valid_char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-        valid_char_map = {}
-        for char in valid_char:
-            valid_char_map[char] = True
+        valid_char_map = set(list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'))
 
         while leading_index < trailing_index:
-            leading_char = s[leading_index]
-            trailing_char = s[trailing_index]
-
-            if leading_char not in valid_char_map:
+            if s[leading_index] not in valid_char_map:
                 leading_index += 1
-
-            if trailing_char not in valid_char_map:
+            elif s[trailing_index] not in valid_char_map:
                 trailing_index -= 1
-
-            if trailing_char in valid_char_map and leading_char in valid_char_map:
-                if leading_char.lower() == trailing_char.lower():
-                    leading_index += 1
+            else:
+                if s[leading_index].lower() == s[trailing_index].lower():
+                    leading_index +=1
                     trailing_index -= 1
                 else:
                     return False
-
 
         return True
 
