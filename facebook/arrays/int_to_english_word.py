589cc4b53847a0241669fd842206c9b3d7dde471
Nguyen Le <nathanle89@gmail.com>
2018-09-05 10:22:23 -0700

    Implement int to words

--- facebook/arrays/int_to_english_word.py
+++ facebook/arrays/int_to_english_word.py
@@ -27,7 +27,7 @@ class Solution(object):
         :rtype: str
         """
 
-        n2w = {1e9: "Billion", 1e6: "Million", 1e3: "Thousand", 1e2: "Hundred",
+        mapping = {1e9: "Billion", 1e6: "Million", 1e3: "Thousand", 1e2: "Hundred",
                90:  "Ninety", 80:  "Eighty", 70:  "Seventy",
                60:  "Sixty", 50:  "Fifty", 40:  "Forty",
                30:  "Thirty", 20:  "Twenty", 19: "Nineteen",
@@ -38,17 +38,35 @@ class Solution(object):
                6:   "Six", 5:   "Five", 4: "Four", 3: "Three",
                2: "Two", 1: "One", 0: "Zero"}
 
-        keys = [1000000000, 1000000, 1000, 100, 90, 80, 70,
-                60, 50, 40, 30, 20, 19, 18, 17, 16, 15, 14, 13, 12,
-                11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+        sorted_key = [1e9,1e6,1e3,1e2,90,80,70,60,50,40,30,20, 19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
 
-        def dp(n):
-            if n <= 20: return n2w[n]
-            for div in keys:
-                d, r = divmod(n, div)
-                if not d: continue
-                s1 = dp(d) + " " if div >= 100 else ""
-                s2 = " " + dp(r) if r else ""
-                return s1 + n2w[div] + s2
+        return self.helper(num, mapping, sorted_key)
 
-        return dp(num)
+    def helper(self, num, mapping, sorted_key):
+        front = ""
+        back = ""
+        for divider in sorted_key:
+            if num <= 20:
+                return mapping[num]
+            d, r = divmod(num, divider)
+            d = int(d)
+            r = int(r)
+            if not d:
+                continue
+
+            if divider >= 100:
+                front += self.helper(d, mapping, sorted_key) + " "
+            else:
+                front += ""
+
+            if r:
+                back += " " + self.helper(r, mapping, sorted_key)
+            else:
+                back += ""
+
+            return front + mapping[divider] + back
+
+solution = Solution()
+
+print(solution.numberToWords(3295))
+
